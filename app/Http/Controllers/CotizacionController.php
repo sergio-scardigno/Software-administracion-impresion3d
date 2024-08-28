3a52734943dea0628602e2f3c128853a4f1fcc44
Sergio <sergioscardigno82@gmail.com>
2024-08-28 14:21:05 -0300

    ajustando la cotizacion dolar

--- app/Http/Controllers/CotizacionController.php
+++ app/Http/Controllers/CotizacionController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Services\DolarCotizacionService;
 use App\Models\Cotizacion;
+use Carbon\Carbon;
+
 
 class CotizacionController extends Controller
 {
@@ -16,18 +18,26 @@ class CotizacionController extends Controller
 
     public function obtenerCotizacion()
     {
+        $fechaActual = Carbon::today(); // Obtiene solo la fecha de hoy (hora establecida a 00:00:00)
+    
         $valorDolar = $this->dolarService->obtenerCotizacionDolar();
-
+    
         if ($valorDolar) {
-            // Guardar la cotización en la base de datos
-            Cotizacion::create([
-                'valor_dolar' => $valorDolar,
-                'fuente' => 'Dolar API',
-            ]);
-
+            // Obtener las cotizaciones del mismo día
+            $ultimaCotizacion = Cotizacion::whereDate('created_at', $fechaActual)->first();
+    
+            if (!$ultimaCotizacion) {
+                // Guardar la cotización en la base de datos solo si no hay una cotización para hoy
+                Cotizacion::create([
+                    'valor_dolar' => $valorDolar,
+                    'fuente' => 'Dolar API',
+                ]);
+            }
+    
             return response()->json(['valor_dolar' => $valorDolar]);
         }
-
+    
         return response()->json(['error' => 'No se pudo obtener la cotización'], 500);
     }
+    
 }
